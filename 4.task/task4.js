af424e590dca540f84279c2b58e8326cac6d38ac
Alex Z <azhernovyi@gmail.com>
2018-07-21 09:20:28 +0300

    Add files via upload

---
+++ 4.task/task4.js
@@ -0,0 +1,14 @@
+function recurs(n, str) {
+    console.log(str);
+    if (n !== 0) {
+       recurs(n - 1, str + '#');
+    }
+}
+recurs(7, '#');
+
+
+var arr = '#';
+for (var i = 0; i <= 7; i++) {
+	arr += '#';
+	console.log(arr);
+}
