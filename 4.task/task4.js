4f03abd1d30206cb9d63bf0c5bb956a50b88d55f
Alex Z <azhernovyi@gmail.com>
2018-07-21 09:22:38 +0300

    Print in console using for

---
+++ 4.task/task4.js
@@ -0,0 +1,15 @@
+
+var arr = '#';
+for (var i = 0; i <= 7; i++) {
+	arr += '#';
+	console.log(arr);
+}
+
+function recurs(n, str) {
+    console.log(str);
+    if (n !== 0) {
+       recurs(n - 1, str + '#');
+    }
+}
+recurs(7, '#');
+
